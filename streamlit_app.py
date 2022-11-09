a0da38c08130e290c639256569f5bc879cfa2866
ztlock <speedyzack@yahoo.com>
2022-11-09 11:57:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+import urllib.error import URLError
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
@@ -46,4 +47,4 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfru
 streamlit.write('Thank you for adding ', add_my_fruit)
 
 
-
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
