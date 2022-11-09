eca4ed3f32a3041500fa0d04673d261eeeaa0a5d
ztlock <speedyzack@yahoo.com>
2022-11-09 12:02:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-import urllib.error import URLError
+from urllib.error import URLError
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
