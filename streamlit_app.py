78583e4cbd7ee56cae9f0d58047dd67410200e80
ztlock <speedyzack@yahoo.com>
2022-11-09 09:30:37 -0600

    add fruit input textbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,8 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thank you for adding ', add_my_fruit)
+
+
 
